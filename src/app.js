76a8c0127a60492b6ef5de0d20caa11dec458a73
Alonso Holmes <hello@alonso.io>
2016-03-05 00:07:21 -0800

    got show/hide animations working

--- src/app.js
+++ src/app.js
@@ -35,6 +35,8 @@ if (__DEV__) {
     Mixpanel.sharedInstanceWithToken('e427bf5ada34e28eced40b58b6c468f9');
 }
 
+import EmojiSploder from './components/EmojiSploder/EmojiSploder';
+
 export default class App extends Component {
 
     componentDidMount() {
@@ -42,6 +44,7 @@ export default class App extends Component {
     }
 
     render() {
+        return <EmojiSploder />
         return <Auth />;
     }
 }
