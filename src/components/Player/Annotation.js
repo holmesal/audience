afd6f7a4f4841f70cf4a39cbfae178497d8c606e
Alonso Holmes <hello@alonso.io>
2016-03-09 23:30:41 -0800

    in-flight mutations now show a loading spinner

---
+++ src/components/Player/Annotation.js
@@ -0,0 +1,41 @@
+import React, {
+    Component,
+    Image,
+    PropTypes,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import Relay from 'react-relay';
+
+class Annotation extends Component {
+
+    render() {
+        return (
+            <View style={styles.wrapper}>
+                <Text>[{this.props.annotation.time}] {this.props.annotation.user.displayName} : {this.props.annotation.text}</Text>
+            </View>
+        );
+    }
+}
+
+let styles = StyleSheet.create({
+    wrapper: {
+        flex: 1
+    }
+});
+
+export default Relay.createContainer(Annotation, {
+    fragments: {
+        annotation: () => Relay.QL`
+            fragment on Annotation {
+                id
+                text
+                time
+                user {
+                    displayName
+                }
+            }
+        `
+    }
+});
