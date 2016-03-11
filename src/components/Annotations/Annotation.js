93434ff077de5cd550232ef3e312de3878557ed8
Alonso Holmes <hello@alonso.io>
2016-03-11 13:49:40 -0800

    cleaning up

--- src/components/Annotations/Annotation.js
+++ src/components/Annotations/Annotation.js
@@ -20,38 +20,13 @@ class Annotation extends Component {
             <Comment
                 avatar={<FacebookAvatar user={this.props.annotation.user} size={32}/>}
                 name={this.props.annotation.user.displayName}
-                text={`${this.props.annotation.text} [${prettyFormatTime(Math.round(this.props.annotation.time))}`}
+                text={this.props.annotation.text}
             />
         );
     }
 }
 
 let styles = StyleSheet.create({
-    //wrapper: {
-    //    flexDirection: 'row',
-    //    paddingLeft: 12,
-    //    alignSelf: 'stretch',
-    //    paddingBottom: 12,
-    //    marginBottom: 6
-    //},
-    //photo: {
-    //    marginRight: 12
-    //},
-    //content: {
-    //    flex: 1
-    //},
-    //name: {
-    //    fontFamily: 'System',
-    //    color: colors.grey,
-    //    fontWeight: '500',
-    //    letterSpacing: 0.3
-    //},
-    //text: {
-    //    fontFamily: 'Charter',
-    //    color: colors.darkGrey,
-    //    fontSize: 18,
-    //    lineHeight: 24
-    //}
 });
 
 export default Relay.createContainer(Annotation, {
